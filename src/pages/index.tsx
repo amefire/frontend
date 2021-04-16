9615a505ceac963b3febe7322f6d2d562af9877a
amefire <abdallahmefire@gmail.com>
2021-04-17 05:54:13 +0600

    logout working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,15 @@ import {
   ListIcon,
   ListItem,
 } from '@chakra-ui/react'
-// import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-// import { Hero } from '../components/Hero'
-// import { Container } from '../components/Container'
-// import { Main } from '../components/Main'
-// import { DarkModeSwitch } from '../components/DarkModeSwitch'
-// import { CTA } from '../components/CTA'
-// import { Footer } from '../components/Footer'
+import { NavBar } from '../components/NavBar'
+
 
 const Index = () => (
+  <>
+  <NavBar/>
   <div>hello world</div>
+
+  </>
 )
 
 export default Index
